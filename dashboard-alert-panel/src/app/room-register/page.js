5c358f476268bd4938d58cd217ba654bbb6451a9
LEANDRO MORAIS DA FROTA <LEANDRO9863@DIGITALCOLLEGE.LOCAL>
2024-08-21 20:18:57 -0300

    Refactored room-register page

--- dashboard-alert-panel/src/app/room-register/page.js
+++ dashboard-alert-panel/src/app/room-register/page.js
@@ -6,52 +6,64 @@ import { useState } from "react";
 
 export default function RoomRegister(){
     const [roomFields, setRoomFields] = useState([
-        {
-            editable: false,
-            value: ""
-        }
+       
     ]);
 
-    const [valueInput, setValueInput] = useState(false)
+
 
     function switchEditable(fieldIdx){
         const newRoomFields = roomFields.map((field,idx)=>{
+            const isEmpity = field.value === "";
             if(idx === fieldIdx){
                 return {
                     ...field,
-                    editable : !field.editable
+                    editable : !isEmpity ? !field.editable : field.editable
+                }
+
+                }else{
+                    return{
+                        ...field,
+                        editable: false
+                    }
                 }
             }
             
-            return field;
-        })
+        )
 
         setRoomFields(newRoomFields);
     
     }
 
     function addField(){
-        if(roomFields.every(field => !field.editable && valueInput)){
-            setRoomFields ([...roomFields, {editable: false, value: ""}])
-            setValueInput(false)
+        if(roomFields.every(field => !field.editable && field.value)){
+            setRoomFields ([...roomFields, {editable: true, value: ""}])
+            
         }
     }
 
-    function deleteField(fieldIdx){
+    function deleteField(e){
         
-            const newRoomFieldsFilter = roomFields.filter((field,id)=> (id !== fieldIdx || fieldIdx == 0))
-
-                        
-            setRoomFields(newRoomFieldsFilter)
-            setValueInput(true)
-            console.log(fieldIdx)   
-            console.log(newRoomFieldsFilter)
-            
-            
+            const newRoomFieldsFilter = roomFields.filter((field,id)=> id !== Number(e.target.id))
+                       
+            setRoomFields(newRoomFieldsFilter)    
+    
         }
 
-        console.log(valueInput)
-    
+        function onChangeField(e,idx){
+            const newRoomFields = roomFields.map((field, fieldIdx)=>{
+                if(fieldIdx === idx){
+                    return{
+                        ...field,
+                        value: e.target.value
+
+                    }
+                }
+                return field;
+            })
+            setRoomFields(newRoomFields)
+        }
+
+   
     return (
         <main className="flex min-h-screen gap-4 flex-grow flex-col items-start justify-start p-6">
              <h1 className="text-2xl font-bold">Registrar sala </h1>
@@ -67,9 +79,9 @@ export default function RoomRegister(){
              </div>
              {roomFields.map((field,fieldIdx)=>{
                 return <div className="flex gap-2" key={fieldIdx}>
-                        <input onChange={() => setValueInput(true)}  className="flex gap-2 disabled:bg-gray-300" disabled={!field.editable}/>
+                        <input  onChange={(event) => onChangeField(event,fieldIdx)}  className="flex gap-2 disabled:bg-gray-300" disabled={!field.editable}/>
                         <Pen onClick={()=> switchEditable(fieldIdx)} size={20} className="text-esmerald-800 cursor-pointer"/>
-                        <Trash onClick={()=> deleteField(fieldIdx)} size={20} className="text-esmerald-800 cursor-pointer"/>
+                        <Trash id={fieldIdx} onClick={deleteField} size={20} className="text-esmerald-800 cursor-pointer"/>
                     </div>
              })}
 
